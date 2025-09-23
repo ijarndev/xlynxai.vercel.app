f22a656821b1b183c37af3089c53f9b48162cfc3
Randy J <ijarndev@gmail.com>
2025-09-23 15:21:41 -0400

    feat: newsletter ✔

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,5 +1,15 @@
 import { defineAction } from 'astro:actions';
 import { z } from 'astro:schema';
+import { Resend } from 'resend'
+
+const resend = new Resend('re_MfmsaiYu_JR5i87RjPhnEJnzeTqXunFyY');
+
+/*
+import { Resend } from 'resend';
+
+
+
+*/
 
 export const server = {
   subscribeToNewsletter: defineAction({
@@ -9,9 +19,17 @@ export const server = {
     handler: async (input) => {
       const { email } = input
 
-      return {
-        email,
-        success: true
+      try {
+        resend.contacts.create({
+          email,
+          unsubscribed: false,
+          audienceId: '53064908-ec13-47ee-b942-8d98d3dc7483',
+        });
+
+        return { email }
+      } catch (e) {
+        return { error: 'Failed to subscribe, please try again later.' }
+        console.log(e)
       }
     }
   })

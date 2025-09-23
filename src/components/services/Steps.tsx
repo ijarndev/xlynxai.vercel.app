f936dac781023bf00a1e89af639a5f08d6a5a126
Randy J <ijarndev@gmail.com>
2025-09-22 22:43:37 -0400

    enhancement: minor tasks

---
+++ src/components/services/Steps.tsx
@@ -0,0 +1,63 @@
+import { motion, useScroll, useSpring } from "framer-motion"
+import { useEffect, useState } from "react"
+
+export default function Steps () {
+  const { scrollYProgress } = useScroll()
+  const scale = useSpring(scrollYProgress, {
+    stiffness: 100,
+    damping: 100,
+    restDelta: 0.001
+  })
+
+  return (
+    <div className="mt-30 grid gap-10 md:grid-cols-4 md:gap-0 relative">
+        <div className="hidden md:block w-full h-2 bg-zinc-600 rounded-full overflow-hidden absolute left-0 top-6">
+          <motion.div style={{ scaleX: scale }} className={`h-full background-theme rounded-full`} />
+        </div>
+
+        <div>
+          <StepItem
+            number={1}
+            title="Free Consultation"
+            description="We analyze your needs and discuss your vision"
+          />
+        </div>
+
+        <StepItem
+          number={2}
+          title="Design & Customization"
+          description="We adapt the website to your brand and style"
+        />
+
+        <StepItem
+          number={3}
+          title="Delivery & Training"
+          description="We teach you how to use it effectively"
+        />
+
+        <StepItem
+          number={4}
+          title="Initial Support"
+          description="First 30 days for basic adjustments"
+        />
+      </div>
+  )
+} 
+
+export function StepItem ({
+  number, title, description
+} : {
+  number: number, title: string, description: string
+}) {
+  return (
+    <div className="relative flex flex-col items-center">
+      <div className="flex h-14 w-14 items-center justify-center rounded-full background-theme text-xl font-bold text-zinc-100">
+        {number}
+      </div>
+      <div className="mt-4 text-center max-w-[300px]">
+        <h3 className="text-xl font-bold text-zinc-100">{title}</h3>
+        <p className="mt-2 text-zinc-300">{description}</p>
+      </div>
+    </div>
+  )
+}
